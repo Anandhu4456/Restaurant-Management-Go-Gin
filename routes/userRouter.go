76537de6ea3c9e32b32fdf4447fadc5c1930cb6c
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-02 15:08:12 +0530

    user routes structured with functions

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/Anandhu4456/go-restaurant-management/controller"
+	"github.com/gin-gonic/gin"
+)
+
+
+func UserRoutes(incomingRoute *gin.Engine){
+	incomingRoute.GET("/users",controller.GetUsers())
+	incomingRoute.GET("/users/:user_id",controller.GetUser())
+	incomingRoute.POST("/users/signup",controller.Signup())
+	incomingRoute.POST("/users/login",controller.Login())
+}
