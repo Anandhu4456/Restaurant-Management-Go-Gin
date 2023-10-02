54c9db669b9f1069819c870638447bc6e498dc38
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-02 14:54:45 +0530

    mentioned route functions

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"os"
+
+	"github.com/Anandhu4456/go-restaurant-management/database"
+	"github.com/Anandhu4456/go-restaurant-management/middleware"
+	"github.com/Anandhu4456/go-restaurant-management/routes"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client,"food")
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+
+	router.Use(gin.Logger())
+	router.Use(middleware.Authentication())
+
+	routes.UserRoutes(router)
+	routes.FoodRoutes(router)
+	routes.InvoiceRoutes(router)
+	routes.MenuRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.TableRoutes(router)
+
+	router.Run(":" + port)
+}
