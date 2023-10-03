396a34a42669eae2392c5a1dd0540af08cb0dbcb
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-03 21:03:40 +0530

    food controller functions created

--- controller/foodController.go
+++ controller/foodController.go
@@ -1,1 +1,27 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetAllFoods()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func GetOneFoodItem()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func CreateFood()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func UpdateFood()gin.HandlerFunc{
+	return func(c *gin.Context){
+		
+	}
+}
