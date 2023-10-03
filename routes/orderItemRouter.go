c22918f16a5b9f688960831f9fc98636e16369c7
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-03 19:39:45 +0530

    created order item routes and functions

--- routes/orderItemRouter.go
+++ routes/orderItemRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	controller"github.com/Anandhu4456/go-restaurant-management/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func OrderItemRoutes(incomingRoute *gin.Engine){
+	incomingRoute.GET("/orderItems",controller.GetOrderItems())
+	incomingRoute.GET("/orderItems/:orderItem_id",controller.GetOneOrderItem())
+	incomingRoute.GET("/orderItems-order",controller.GetOrderItemsByOrder())
+	incomingRoute.POST("/orderItems",controller.CreateOrderItem())
+	incomingRoute.PATCH("/orderItems/:orderItem_id",UpdateOrderItem())
+}
