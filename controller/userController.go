825e8f3340c5411de2912607f6b2d380dc403bbc
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-03 20:41:07 +0530

    created user controller functions

--- controller/userController.go
+++ controller/userController.go
@@ -1,1 +1,27 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetUser()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func Signup()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func Login()gin.HandlerFunc{
+	return func(c *gin.Context){
+		
+	}
+}
