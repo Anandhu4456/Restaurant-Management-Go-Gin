3bfadbd9e93374f76fc3dfb921d27bb68b143c94
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-04 12:42:00 +0530

    created table route functions and order item function

--- controller/orderItemController.go
+++ controller/orderItemController.go
@@ -1,1 +1,40 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetOneOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetOrderItemsByOrder()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func ItemsByOrderId(id string) (OrderItems []primitive.M, err error) {
+
+}
+
+func CreateOrderItem()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func UpdateOrderItem()gin.HandlerFunc{
+	return func(c *gin.Context){
+		
+	}
+}
--- controller/tableController.go
+++ controller/tableController.go
@@ -1,1 +1,27 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetAllTables()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func GetOneTable()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func CreateTable()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func UpdateTable()gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
