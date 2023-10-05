ee9dbbea92fe0f5cf5876237eebe0cb5ab940403
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-05 23:42:36 +0530

    get all menu function is created in menu controller

--- controller/menuController.go
+++ controller/menuController.go
@@ -1,27 +1,49 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Anandhu4456/go-restaurant-management/database"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc {
-	return func(c *gin.Context){
+	return func(c *gin.Context) {
+		var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+		result,err:=menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured when listing the menu items"})
+		}
+		var allMenus []bson.M
+		if err = result.All(ctx,&allMenus);err!=nil{
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK,allMenus)
+	}
+}
+
+func GetOneMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }
 
-func GetOneMenu()gin.HandlerFunc{
-	return func(c *gin.Context){
+func CreateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }
 
-func CreateMenu()gin.HandlerFunc{
-	return func(c *gin.Context){
+func UpdateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-	}
-}
-
-func UpdateMenu()gin.HandlerFunc{
-	return func(c *gin.Context){
-		
 	}
 }
