553273a75ef675a50a0e5b05df0426579a5a5b78
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-05 22:45:31 +0530

    get one food item function created in food controller

--- controller/foodController.go
+++ controller/foodController.go
@@ -1,6 +1,19 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/Anandhu4456/go-restaurant-management/database"
+	"github.com/Anandhu4456/go-restaurant-management/model"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client,"food")
+var err error
 
 func GetAllFoods()gin.HandlerFunc{
 	return func(c *gin.Context){
@@ -10,7 +23,16 @@ func GetAllFoods()gin.HandlerFunc{
 
 func GetOneFoodItem()gin.HandlerFunc{
 	return func(c *gin.Context){
-
+		var ctx,cancel =context.WithTimeout(context.Background(),100*time.Second)
+		foodId :=c.Param("food_id")
+		var food model.Food
+		foodCollection.FindOne(ctx,bson.M{"food_id":foodId}).Decode(&food)
+		defer cancel()
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured when fetching the food item"})
+			
+		}
+		c.JSON(http.StatusOK,food)
 	}
 }
 
