7d396e8ea5957f332130646c56b0c7b958225893
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-06 13:45:19 +0530

    create menu function created in menu controller

--- controller/menuController.go
+++ controller/menuController.go
@@ -10,6 +10,7 @@ import (
 	"github.com/Anandhu4456/go-restaurant-management/model"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -36,19 +37,44 @@ func GetOneMenu() gin.HandlerFunc {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		var menu model.Menu
 		menuId := c.Param("menu_id")
-		err:=menuCollection.FindOne(ctx,bson.M{"menu_id":menuId}).Decode(&menu)
-		if err!=nil{
-			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured when fetching the menu"})
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": menuId}).Decode(&menu)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured when fetching the menu"})
 			return
 		}
 		defer cancel()
-		c.JSON(http.StatusOK,menu)
+		c.JSON(http.StatusOK, menu)
 	}
 }
 
 func CreateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var menu model.Menu
+		err:=c.BindJSON(menu)
+		if err!=nil{
+			c.JSON(http.StatusBadRequest,bson.M{"error":err.Error()})
+			return
+		}
+		
+		validateError:=validate.Struct(menu)
+		if validateError!=nil{
+			c.JSON(http.StatusBadRequest,bson.M{"error":err.Error()})
+			return
+		}
 
+		menu.Created_at =time.Now()
+		menu.Updated_at = time.Now()
+		menu.ID = primitive.NewObjectID()
+		menu.Menu_id = menu.ID.Hex()
+		
+		result,insertErr:=menuCollection.InsertOne(ctx,menu)
+		if insertErr!=nil{
+			c.JSON(http.StatusInternalServerError,bson.M{"error":"error while creating menu"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK,result)
 	}
 }
 
