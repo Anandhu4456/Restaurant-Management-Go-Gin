b11fa19f83825cf208515f30dd7179600f949f74
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-07 13:41:45 +0530

    updated order item

--- controller/orderController.go
+++ controller/orderController.go
@@ -9,7 +9,9 @@ import (
 	"github.com/Anandhu4456/go-restaurant-management/model"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
@@ -56,6 +58,49 @@ func CreateOrder() gin.HandlerFunc {
 
 func UpdateOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
 
+		orderId:=c.Param("order_id")
+		var order model.Order
+		var table model.Table
+		err:=c.BindJSON(&order)
+		if err!=nil{
+			c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+			return
+		}
+		var updateObj primitive.D
+
+		if order.Table_id!=nil{
+		err:=orderCollection.FindOne(ctx,bson.M{"table_id":order.Table_id}).Decode(&table)
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"could't find table"})
+			return
+		}
+		updateObj = append(updateObj, bson.E{Key:"table_id",Value:order.Table_id})
+		}
+		order.Updated_at = time.Now()
+		updateObj = append(updateObj, bson.E{Key:"updated_at",Value:order.Updated_at})
+
+		upsert:=true
+		filter:=bson.M{"order_id":orderId}
+		
+		opt:=options.UpdateOptions{
+			Upsert: &upsert,
+		}
+		result,updateError:=orderCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{Key: "$set",Value: updateObj},
+			},
+			&opt,
+		)
+		if updateError!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"couldn't update order"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK,result)
+		
 	}
 }
