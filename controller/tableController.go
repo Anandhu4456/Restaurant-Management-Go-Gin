bae00586755d952fb3ace1a96a6ce98b5349284c
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-09 21:34:25 +0530

    getall tables completed

--- controller/tableController.go
+++ controller/tableController.go
@@ -1,27 +1,49 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
 
-func GetAllTables()gin.HandlerFunc{
-	return func(c *gin.Context){
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// var tableCollection *mongo.Collection = database.OpenCollection(database.Client,"table")
+
+func GetAllTables() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result,err:=tableCollection.Find(context.TODO(), bson.M{})
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"error occured when listing tables"})
+			return
+		}
+		var allTables []bson.M
+		err = result.All(ctx,&allTables)
+		if err!=nil{
+			log.Fatal(err)
+		}
+		defer cancel()
+		c.JSON(http.StatusOK,result)
+	}
+}
+
+func GetOneTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }
 
-func GetOneTable()gin.HandlerFunc{
-	return func(c *gin.Context){
+func CreateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }
 
-func CreateTable()gin.HandlerFunc{
-	return func(c *gin.Context){
-
-	}
-}
-
-func UpdateTable()gin.HandlerFunc{
-	return func(c *gin.Context){
+func UpdateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
 	}
 }
