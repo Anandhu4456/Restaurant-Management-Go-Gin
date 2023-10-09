690ffc4bf703148d2e3849eb1f6b1d3199415511
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-09 23:14:10 +0530

    table updation completed

--- controller/tableController.go
+++ controller/tableController.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // var tableCollection *mongo.Collection = database.OpenCollection(database.Client,"table")
@@ -65,7 +66,7 @@ func CreateTable() gin.HandlerFunc {
 		table.Updated_at = time.Now()
 		table.ID = primitive.NewObjectID()
 		table.Table_id = table.ID.Hex()
-		// table.Table_number =
+
 		result, insertError := tableCollection.InsertOne(ctx, table)
 		if insertError != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "table creation failed"})
@@ -78,6 +79,47 @@ func CreateTable() gin.HandlerFunc {
 
 func UpdateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		tableId := c.Param("table_id")
+		var table model.Table
+
+		err := c.BindJSON(&table)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var updateObj primitive.D
+
+		if table.Number_of_guests!=nil{
+			updateObj = append(updateObj, bson.E{Key: "number_of_guests",Value: table.Number_of_guests})
+		}
+		if table.Table_number!=nil{
+			updateObj = append(updateObj, bson.E{Key:"table_number",Value: table.Table_number})
+		}
+		table.Updated_at = time.Now()
+		updateObj = append(updateObj, bson.E{Key: "updated_at",Value: table.Updated_at})
+
+		upsert:=true
+		filter:=bson.M{"table_id":tableId}
+
+		opt:=options.UpdateOptions{
+			Upsert: &upsert,
+		}
+		result,updateError:=tableCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{Key: "$set",Value: updateObj},
+			},
+			&opt,
+		)
+		if updateError!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"table updation failed"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK,result)
 	}
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/validator/v10 v10.14.0
 	go.mongodb.org/mongo-driver v1.12.1
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
 )
 
 require (
@@ -36,7 +37,7 @@ require (
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
