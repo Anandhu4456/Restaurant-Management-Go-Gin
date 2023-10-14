2bfdd56b9ba3372e9e634d4253737c3e5e2ff0d6
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-14 23:05:07 +0530

    signed details struct and secret key setted

--- helper/tokenHelper.go
+++ helper/tokenHelper.go
@@ -1,5 +1,23 @@
 package helper
 
+import (
+	"os"
+
+	"github.com/Anandhu4456/go-restaurant-management/database"
+	"github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SignedDetails struct{
+	Email string
+	First_name string
+	Last_name string
+	Uid string
+	jwt.StandardClaims
+}
+var userCollection *mongo.Collection = database.OpenCollection(database.Client,"user")
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
 func GenerateAllToken(){
 	
 }
