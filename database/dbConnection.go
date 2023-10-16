9a1f5be60cfcd276b9814d92026f1cc7f6150fb3
Anandhu4456 <anandhurajan56@gmail.com>
2023-10-16 15:27:43 +0530

    added env file

--- database/dbConnection.go
+++ database/dbConnection.go
@@ -11,7 +11,7 @@ import (
 )
 
 func DbInstance() *mongo.Client {
-	MongoDb := "mongodb://localhost:27017"
+	MongoDb := "mongodb://localhost:27017/restaurant"
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
 	if err != nil {
@@ -24,7 +24,7 @@ func DbInstance() *mongo.Client {
 
 	err = client.Connect(ctx)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("couldn't connect ",err)
 	}
 	fmt.Println("Connected to mongodb....")
 	return client
--- model/userModel.go
+++ model/userModel.go
@@ -6,17 +6,18 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type User struct{
-	ID primitive.ObjectID `bson:"_id"`
-	First_name *string `json:"first_name" validate:"required,min=2,max=20"`
-	Last_name *string `json:"last_name" validate:"required,min=2,max=20"`
-	Phone *string `json:"phone" validate:"required"`
-	Email *string `json:"email" validate:"email,required"`
-	Password *string `json:"password" validate:"required,min=6"`
-	Avatar *string `json:"avatar"`
-	Token *string `json:"token"`
-	Refresh_token *string `json:"refresh_token"`
-	Created_at time.Time `json:"created_at"`
-	Updated_at time.Time `json:"updated_at"`
-	User_id string `json:"user_id"`
+type User struct {
+	ID            primitive.ObjectID `bson:"_id"`
+	First_name    *string            `json:"first_name" validate:"required,min=2,max=20"`
+	Last_name     *string            `json:"last_name" validate:"required,min=2,max=20"`
+	Phone         *string            `json:"phone" validate:"required"`
+	Email         *string            `json:"email" validate:"email,required"`
+	Password      *string            `json:"password" validate:"required,min=6"`
+	Avatar        *string            `json:"avatar"`
+	Token         *string            `json:"token"`
+	Refresh_token *string            `json:"refresh_token"`
+	Created_at    time.Time          `json:"created_at"`
+	Updated_at    time.Time          `json:"updated_at"`
+	User_id       string             `json:"user_id"`
+	// User_type     string             `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
 }
